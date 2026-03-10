7ee7de76604b546d346237ea14737e49f08cc5d4
Lopez Garcia Mario <mariolopez03@uv.mx>
2026-03-10 13:52:58 -0600

    Mi primer commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EjercicioActions.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
